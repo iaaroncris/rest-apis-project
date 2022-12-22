ed7cb3e71eaf4d4fbc383d1d86c615b8150fba12
Aaron Araujo <aaron.araujo@hotmail.com>
2022-12-22 14:41:44 +0530

    Minor changes

--- app.py
+++ app.py
@@ -24,8 +24,9 @@ def create_app(db_url=None):
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite3:///data.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
     migrate = Migrate(app,db)
     api = Api(app)
