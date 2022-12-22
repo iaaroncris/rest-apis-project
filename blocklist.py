0967384d295060fe0d9d37a750d721848b9ce67e
Aaron Araujo <aaron.araujo@hotmail.com>
2022-12-22 10:25:44 +0530

    Creation of REST API for store management.

---
+++ blocklist.py
@@ -0,0 +1,11 @@
+# from models import BlocklistModel
+# from db import db
+
+BLOCKLIST = set()
+# new_list = list(BLOCKLIST)
+# db.session.add_all(new_list)
+# db.session.commit()
+
+# result = db.session.query(BlocklistModel.token).all()
+# result = [token for token, in result]
+# BLOCKLIST = set(result)
